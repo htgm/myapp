fc623164d4726e868cfb080cbb94355542bec93e
htgm <n/a>
2018-07-21 06:26:23 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainMenu from './MainMenu';
+import TipsBallon from './TipsBalloon';
 
 class App extends Component {
   render() {
@@ -14,7 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <MainMenu />
+        <MainMenu/>
+        <TipsBallon id="TipsBalloon"/>
       </div>
     );
   }
--- src/TipsBalloon.js
+++ src/TipsBalloon.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+import './TipsBalloon.css';
+
+export default class TipsBallon extends Component {
+  render() {
+    return (
+      <div className="TipsBalloon">
+        <h2>head</h2>
+        QWERTY
+      </div>
+    );
+  }
+}
